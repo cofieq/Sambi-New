fba287d19d8f140b48fad0b3e4bb5544247daccd
sambiproject.id <sambiproject.id@gmail.com>
2026-01-27 15:00:20 +0000

    melanjutkan project

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'com.example.app',
+  appName: 'chefstock-pro---smart-kitchen-inventory',
+  webDir: 'dist'
+};
+
+export default config;
